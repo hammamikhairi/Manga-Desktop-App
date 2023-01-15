527c25a77e1a76ffa21e3bdc6a7e7ecbba179a77
HammamiKhairi <hammamikhairi@gmail.com>
2023-01-15 19:33:31 +0100

    added license

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,13 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
+// import {useState} from 'react';
+// import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet, Prompt} from "../wailsjs/go/main/App";
+// import {Greet, Prompt} from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-        // Prompt().then(res => console.log(res))
-    }
-
-    function prompt() {
-        Prompt().then(updateResultText)    
-    }
-    
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={prompt}>Greet</button>
-            </div>
+            
         </div>
     )
 }
