c922b893496320c2412d8b2d78a19d815a665896
HammamiKhairi <hammamikhairi@gmail.com>
2023-01-16 03:10:12 +0100

    created navbar

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { IoNotificationsOutline, IoNotificationsSharp } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+import './navbar.sass';
+
+const getPath = () => {
+  return "/" + window.location.href.split("/").pop() || "/"
+}
+
+const Navbar = () => {
+
+  // TODO : notifications system
+  const [notif, setNotif] = useState(true)
+
+  const [location, setLocation] = useState("/")
+
+  return (
+    <div id="navbar" className="navbar-container"  >
+      <div className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <h1 id="logo">Manelo</h1>
+          </Link>
+        </div>
+        <ul className="links" onClick={() => {setLocation(getPath())}} >
+          <Link to="/" ><li  className={ location == "/" ? 'highlighted' : '' } >Home</li></Link>
+          <Link to="/mangas"><li className={ location == "/mangas" ? 'highlighted' : '' } >Mangas</li></Link>
+          <Link to="/list" ><li className={ location == "/list" ? 'highlighted' : '' }> List</li></Link>
+        </ul>
+        <div className="notification-container">
+          {/* <AiOutlineSearch /> */}
+          { notif ? <IoNotificationsSharp /> : <IoNotificationsOutline /> }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
