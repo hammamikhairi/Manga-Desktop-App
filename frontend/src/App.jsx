e7a1d4d9ef20cdb6bc0803d0672a3a6a8348b3bc
HammamiKhairi <hammamikhairi@gmail.com>
2023-01-16 03:08:39 +0100

    project setup + some work on home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
 // import {useState} from 'react';
 // import logo from './assets/images/logo-universal.png';
-import './App.css';
+import './app.sass';
 // import {Greet, Prompt} from "../wailsjs/go/main/App";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import List from './Pages/List/List';
+
 
 function App() {
-
     return (
         <div id="App">
-            
+            <Router >
+                <Navbar />
+                <Routes>
+                    <Route exact path="/" element={<Home />}/>
+                    <Route exact path="/list" element={<List />}/>
+                </Routes>
+            </Router>
         </div>
     )
 }
