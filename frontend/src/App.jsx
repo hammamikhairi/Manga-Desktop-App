5c09f138f61e7ab78e5eb932825a84792b8ebb46
HammamiKhairi <hammamikhairi@gmail.com>
2023-01-18 05:42:44 +0100

    better navbar communication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,15 @@
-// import {useState} from 'react';
-// import logo from './assets/images/logo-universal.png';
 import './app.sass';
 // import {Greet, Prompt} from "../wailsjs/go/main/App";
-import { useState } from 'react';
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import List from './Pages/List/List';
 import Mangas from './Pages/Mangas/Mangas';
 import Read from './Pages/Read/Read';
+import Select from './Pages/Select/Select';
 
 
 function App() {
-
-    const [visibleNav, setVisibleNav] = useState(true)
 
     document.addEventListener("reading", (event) => {
       if (event.detail) {
@@ -26,29 +22,19 @@ function App() {
         try {
             document.getElementById("mangas").classList.toggle("blurr")
         }catch{}
-      } else {
-        setVisibleNav(!visibleNav)
       }
-
-      const type = setInterval(() => {
-        if (event.detail)
-            setVisibleNav(!visibleNav)
-        clearInterval(type)
-      }, 2000);
     })
 
     return (
         <div id="App">
             <Router >
-                {
-                    visibleNav &&
-                    <Navbar />
-                }
+                <Navbar />
                 <Routes>
                     <Route exact path="/" element={<Home />}/>
                     <Route exact path="/list" element={<List />}/>
                     <Route exact path="/mangas" element={<Mangas />}/>
-                    <Route exact path="/read/:id" element={<Read />}/>
+                    <Route exact path="/select/:id" element={<Select />}/>
+                    <Route exact path="/read/:mngId/:chapId" element={<Read />}/>
                 </Routes>
             </Router>
         </div>
