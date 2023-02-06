59775a63df003c908249b96dfac81c3fb66b2934
HammamiKhairi <hammamikhairi@gmail.com>
2023-02-06 17:41:44 +0100

    config changes

--- backend/conf/conf.go
+++ backend/conf/conf.go
@@ -2,41 +2,37 @@ package conf
 
 import (
 	"encoding/json"
+	"fmt"
 	"mngapp/backend/utils"
 	"os"
 )
 
-type MangaProgress map[string]string
+// DONE
+
+// TODO :
+//  - maybe add some stuff (theme ?)
+
+const CONF_PATH_HEAD string = "/MngApp/conf.json"
 
 type UserInfo struct {
-	MangasList    []string `json:"mangaList"`
-	MangaProgress `json:"mangaProgress"`
-
-	OutFile   string `json:"outFile"`
+	ConfFile  string `json:"ConfFile"`
 	MangasDir string `json:"mangasDir"`
 	DataDir   string `json:"dataDir"`
-	LastManga string `json:"lastManga"`
 }
 
 func Default() *UserInfo {
 	homeDir, _ := os.UserHomeDir()
-	utils.CheckDirectory(homeDir + "/mngApp")
-	//! this chages
-	utils.CheckDirectory(homeDir + "/mngApp/Data")
-
+	confDir, _ := os.UserConfigDir()
 	return &UserInfo{
-		OutFile:   homeDir + "/mngApp/conf",
+		ConfFile:  confDir + CONF_PATH_HEAD,
 		MangasDir: homeDir + "/mngApp/",
-		//! this chages
-		DataDir:       homeDir + "/mngApp/Data",
-		LastManga:     "",
-		MangasList:    []string{"csm"},
-		MangaProgress: MangaProgress{"csm": "1"},
+		// FIXME : this changes
+		DataDir: homeDir + "/mngApp/Data",
 	}
 }
 
 func (u *UserInfo) Load() {
-	cfgData, err := os.ReadFile(u.OutFile)
+	cfgData, err := os.ReadFile(u.ConfFile)
 
 	if err != nil {
 		return
@@ -45,33 +41,38 @@ func (u *UserInfo) Load() {
 	json.Unmarshal(cfgData, &u)
 }
 
-func AppInit() *UserInfo {
+func (u *UserInfo) CheckConfig() error {
+	// fmt.Println(u.ConfFile)
+	if utils.FileExists(u.ConfFile) {
+		return nil
+	}
+
+	return fmt.Errorf("This Error")
+}
+
+func ConfInit() *UserInfo {
+
 	user := Default()
-	user.Load()
+	if err := user.CheckConfig(); err != nil {
+		// no config -> init
+		fmt.Println("here")
+		confBase, _ := os.UserConfigDir()
+		utils.MakeDir(confBase + "/MngApp")
+		user.Save()
+	} else {
+		user.Load()
+	}
+
+	// checks
+	utils.CheckDirectory(user.DataDir)
+	utils.CheckDirectory(user.MangasDir)
+
 	return user
 }
 
 func (u *UserInfo) Save() {
 	cfgData, _ := json.MarshalIndent(u, "", "  ")
 
-	utils.CheckDirectory(u.MangasDir)
-	err := os.WriteFile(u.OutFile, cfgData, 0644)
+	err := os.WriteFile(u.ConfFile, cfgData, 0644)
 	utils.Check(err)
-}
-
-func (u *UserInfo) AddManga(manga string) {
-	u.MangasList = append(u.MangasList, manga)
-	// ! remmeber this initial isnt always 0
-	u.MangaProgress[manga] = "Chapter 0"
-	u.Save()
-}
-
-func (u *UserInfo) SaveLastManga(manga string) {
-	u.LastManga = manga
-	u.Save()
-}
-
-func (u *UserInfo) UpdateManga(manga string, read string) {
-	u.MangaProgress[manga] = read
-	u.Save()
 }
