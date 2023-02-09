6d38f8783368d0ada27817b1fc977e222379fbdd
HammamiKhairi <hammamikhairi@gmail.com>
2023-02-09 23:27:07 +0100

    started binding back-front

--- app.go
+++ app.go
@@ -3,72 +3,56 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-
-	wsr "github.com/wailsapp/wails/v2/pkg/runtime"
-
 	"net/http"
-	"os"
+
+	managers "mngapp/backend"
+	types "mngapp/backend/Types"
 )
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	buf, err := os.ReadFile("/home/khairi/Pictures/test.jpg")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	w.Header().Set("Content-Type", "image/png")
-	w.Write(buf)
-}
 
 // App struct
 type App struct {
 	ctx context.Context
+	*managers.Managers
 }
 
 // NewApp creates a new App application
 func NewApp() *App {
-	return &App{}
+	return &App{
+		Managers: managers.ManagersInit(),
+	}
 }
 
 // startup is called when the app starts. The context is saved
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
-	handler := http.HandlerFunc(handleRequest)
 
-	http.Handle("/image", handler)
-
-	fmt.Println("Server started at port 8080")
+	// initialize Server
+	http.Handle("/", a.ServerM)
 	go http.ListenAndServe(":8080", nil)
-	// fmt.Println(os.UserConfigDir())
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+// get last manga
+func (a *App) GetLastManga() types.MangaMetaData {
+	// return "nig"
+	res := a.Managers.GetLastManga()
+	fmt.Println(res)
+	return res
 }
 
-func (a *App) Prompt() string {
-	fmt.Println("here!")
-	selection, err := wsr.OpenDirectoryDialog(a.ctx, wsr.OpenDialogOptions{
-		Title:            "It's your turn!",
-		DefaultDirectory: "/home/khairi/",
-	})
+// func (a *App) Prompt() string {
+// 	fmt.Println("here!")
+// 	selection, err := wsr.OpenDirectoryDialog(a.ctx, wsr.OpenDialogOptions{
+// 		Title:            "It's your turn!",
+// 		DefaultDirectory: "/home/khairi/",
+// 	})
 
-	if err != nil {
-		panic("nik")
-	}
+// 	if err != nil {
+// 		panic("nictx
+// 	}
+// initialize Server
 
-	fmt.Println(selection)
+// 	fmt.Printl(selection)
 
-	return selection
-}
-
-func (a *App) Dl() string {
-
-	// :)
-
-	return ""
-}
+// }
+// 	return selection
